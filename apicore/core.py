3e1877ab3f873464431d3806b88972c9f031fed6
Andrey Bychkov <mrdrew@altlinux.org>
2018-07-17 17:32:35 +0300

    Fix attribute namespace

--- apicore/core.py
+++ apicore/core.py
@@ -11,21 +11,21 @@ def get_logger(info):
 
 
 class Service:
-    def __init__(self, buff):
-        self.buff = buff
+    def __init__(self, _buff_):
+        self._buff_ = _buff_
 
     def __pipe_run(self):
-        return Popen([self.src, self.args], stdin=self.buff, stdout=PIPE)
+        return Popen([self.service, self.args], stdin=self._buff_, stdout=PIPE)
 
     def __read(self):
         return self.__pipe_run().stdout.read().decode()
 
     def __buff(self):
-        object.__setattr__(self, 'buff', self.__pipe_run().stdout)
+        setattr(self, 'buff', self.__pipe_run().stdout)
         return self
 
     def pipe(self, set_pipe):
-        setattr(self, 'src', set_pipe['src'])
+        setattr(self, 'service', set_pipe['service'])
         setattr(self, 'args', set_pipe['args'])
         setattr(Service.pipe, 'read', self.__read())
         setattr(Service.pipe, 'buff', self.__buff())
@@ -33,17 +33,18 @@ class Service:
 
 
 class Environment(object):
-    def __init__(self, src=None):
-        self.src = src
+    def __init__(self, _service=None):
+        self._service = _service
 
     def __run(self, args):
-        return Popen([self.src, args], stdout=PIPE)
+        return Popen([self._service, args], stdout=PIPE)
 
     def __read(self):
         return self.__run(self.args).stdout.read().decode()
 
     def __buff(self):
-        return Service(self.__run(self.args).stdout)
+        buff_ = self.__run(self.args).stdout
+        return Service(buff_)
 
     def run(self, args):
         setattr(self, 'args', args)
@@ -56,27 +57,27 @@ class EnvAPI(object):
     __env_attr = {}
 
     def __get_env(self):
-        return Popen([self.__env_attr['src']], stdout=PIPE, stderr=PIPE)
+        return Popen([self.__env_attr['service']], stdout=PIPE, stderr=PIPE)
 
-    def __allowed_src(self, src):
-        self.__env_attr['src'] = src
+    def __allow_service(self):
+        self.__env_attr['service'] = self._service
         return self.__get_env()
 
     def __check_service(self):
         try:
-            self.__allowed_src(self._bin_name)
+            self.__allow_service()
         except:
             return False
         return True
 
-    def service(self, bin_name):
-        setattr(self, '_bin_name', bin_name)
+    def get_api(self, service):
+        setattr(self, '_service', service)
         if not self.__check_service():
             raise AttributeError
-        return Environment(bin_name)
+        return Environment(service)
 
-    def set_pipe(self, bin_name, args):
-        setattr(self, '_bin_name', bin_name)
+    def set_pipe(self, service, args):
+        setattr(self, '_service', service)
         if not self.__check_service():
             raise AttributeError
-        return {'src': bin_name, 'args': args}
+        return {'service': service, 'args': args}
--- tests/test_core.py
+++ tests/test_core.py
@@ -4,32 +4,31 @@ from apicore.core import Environment
 
 def test_service():
     api = EnvAPI()
-    ls = api.service('ls')
+    ls = api.get_api('ls')
     assert isinstance(ls, Environment)
 
 
 def test_api_read():
     api = EnvAPI()
-    ls = api.service('ls')
-    print(type(ls.run('/').read))
+    ls = api.get_api('ls')
     assert 'bin' in ls.run('/').read
 
 
 def test_api_buff():
     api = EnvAPI()
-    ls = api.service('ls')
-    assert ls.run('/').buff.__dict__['buff']
+    ls = api.get_api('ls')
+    assert ls.run('/').buff.__dict__['_buff_']
 
 
 def test_api_set_pipe():
     api = EnvAPI()
     set_pipe = api.set_pipe('grep', 'bin')
-    assert set_pipe['src'] and set_pipe['args']
+    assert set_pipe['service'] and set_pipe['args']
 
 
 def test_api_pipe():
     api = EnvAPI()
-    ls = api.service('ls')
+    ls = api.get_api('ls')
     buff = ls.run('/').buff
     set_pipe = api.set_pipe('grep', 'bin')
     pipe = buff.pipe(set_pipe)

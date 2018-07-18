a8dfe9911f40e6a46fca1100e49f539f821d7f20
Andrey Bychkov <mrdrew@altlinux.org>
2018-07-18 12:18:12 +0300

    Fix tests core

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,34 +2,35 @@ from apicore.core import EnvAPI
 from apicore.core import Environment
 
 
-def test_service():
-    api = EnvAPI()
-    ls = api.get_api('ls')
-    assert isinstance(ls, Environment)
+class TestCore(object):
+    def __init__(self):
+        api = EnvAPI()
+        self.ls = api.get_api('ls')
+        self.grep = api.get_api('grep')
+
+    def test_service(self):
+        assert isinstance(self.ls, Environment)
+
+    def test_api_run(self):
+        assert isinstance(self.ls, Environment)
+
+    def test_api_output(self):
+        assert 'bin' in self.ls.run('/').to_list()
+
+    def test_api_pipe(self):
+        assert 'bin' in self.ls.run('/').pipe(self.grep.run('bin')).to_list()
+
+    def test_recurrent_pipe(self):
+        single_pipe = self.ls.run('/').pipe(self.grep.run('b'))
+        assert 'bin' in single_pipe.pipe(self.grep.run('bin')).to_list()
+
+    def run(self):
+        self.test_service()
+        self.test_api_run()
+        self.test_api_output()
+        self.test_api_pipe()
+        self.test_recurrent_pipe()
 
 
-def test_api_read():
-    api = EnvAPI()
-    ls = api.get_api('ls')
-    assert 'bin' in ls.run('/').read
-
-
-def test_api_buff():
-    api = EnvAPI()
-    ls = api.get_api('ls')
-    assert ls.run('/').buff.__dict__['_buff_']
-
-
-def test_api_set_pipe():
-    api = EnvAPI()
-    set_pipe = api.set_pipe('grep', 'bin')
-    assert set_pipe['service'] and set_pipe['args']
-
-
-def test_api_pipe():
-    api = EnvAPI()
-    ls = api.get_api('ls')
-    buff = ls.run('/').buff
-    set_pipe = api.set_pipe('grep', 'bin')
-    pipe = buff.pipe(set_pipe)
-    assert pipe.__dict__['read'] and pipe.__dict__['buff']
+if __name__ == "__main__":
+    TestCore().run()
